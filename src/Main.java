943ad82c11cd6b9f95324984ce511697ee079d3c
sat0urn <nissanfordo03@gmail.com>
2022-09-29 22:04:24 +0600

    new

--- src/Main.java
+++ src/Main.java
@@ -20,52 +20,68 @@ public class Main {
         String[] dogInfo = new String[4];
 
         while(true) {
-            System.out.println("----------------------------------------");
-            System.out.println("Do you prefer more active, aggressive or calm dog?" +
-                    "\nActive - 1, Aggressive - 2, Calm - 3");
-            System.out.println("----------------------------------------");
-            String mood = scan.nextLine();
-            switch (mood) {
-                case "1" -> dogInfo[0] = new Active().mood();
-                case "2" -> dogInfo[0] = new Aggressive().mood();
-                case "3" -> dogInfo[0] = new Calm().mood();
-                default -> System.out.println("You need to choose between 1 and 3");
+            while(true) {
+                boolean gate = false;
+                System.out.println("----------------------------------------");
+                System.out.println("Do you prefer more active, aggressive or calm dog?" +
+                        "\nActive - 1, Aggressive - 2, Calm - 3");
+                System.out.println("----------------------------------------");
+                String mood = scan.nextLine();
+                switch (mood) {
+                    case "1" -> { dogInfo[0] = new Active().mood(); gate = true; }
+                    case "2" -> { dogInfo[0] = new Aggressive().mood(); gate = true; }
+                    case "3" -> { dogInfo[0] = new Calm().mood(); gate = true; }
+                    default -> System.out.println("You need to choose between 1 and 3");
+                }
+                if (gate) break;
             }
-            System.out.println("----------------------------------------");
-            System.out.println("Pick size of dog that you prefer" +
-                    "\nBig - 1, Medium - 2, Small - 3");
-            System.out.println("----------------------------------------");
-            String size = scan.nextLine();
-            switch (size) {
-                case "1" -> dogInfo[1] = new Big().size();
-                case "2" -> dogInfo[1] = new Medium().size();
-                case "3" -> dogInfo[1] = new Small().size();
-                default -> System.out.println("You need to choose between 1 and 3");
+            while(true) {
+                boolean gate = false;
+                System.out.println("----------------------------------------");
+                System.out.println("Pick size of dog that you prefer" +
+                        "\nBig - 1, Medium - 2, Small - 3");
+                System.out.println("----------------------------------------");
+                String size = scan.nextLine();
+                switch (size) {
+                    case "1" -> { dogInfo[1] = new Big().size(); gate = true; }
+                    case "2" -> { dogInfo[1] = new Medium().size(); gate = true; }
+                    case "3" -> { dogInfo[1] = new Small().size(); gate = true; }
+                    default -> System.out.println("You need to choose between 1 and 3");
+                }
+                if (gate) break;
             }
-            System.out.println("----------------------------------------");
-            System.out.println("Do you like street or domestic animal?" +
-                    "\nStreet - 1, Domestic - 2");
-            System.out.println("----------------------------------------");
-            String lifestyle = scan.nextLine();
-            switch (lifestyle) {
-                case "1" -> dogInfo[2] = new Street().lifestyle();
-                case "2" -> dogInfo[2] = new Domestic().lifestyle();
-                default -> System.out.println("You need to choose between 1 and 2");
+            while(true) {
+                boolean gate = false;
+                System.out.println("----------------------------------------");
+                System.out.println("Do you like street or domestic animal?" +
+                        "\nStreet - 1, Domestic - 2");
+                System.out.println("----------------------------------------");
+                String lifestyle = scan.nextLine();
+                switch (lifestyle) {
+                    case "1" -> { dogInfo[2] = new Street().lifestyle(); gate = true; }
+                    case "2" -> { dogInfo[2] = new Domestic().lifestyle(); gate = true; }
+                    default -> System.out.println("You need to choose between 1 and 2");
+                }
+                if (gate) break;
             }
-            System.out.println("----------------------------------------");
-            System.out.println("Should dog be trained or not trained?" +
-                    "\nTrained - 1, NoTrained - 2");
-            System.out.println("----------------------------------------");
-            String intellect = scan.nextLine();
-            switch (intellect) {
-                case "1" -> dogInfo[3] = new Trained().intellect();
-                case "2" -> dogInfo[3] = new NoTrained().intellect();
-                default -> System.out.println("You need to choose between 1 and 2");
+            while (true) {
+                boolean gate = false;
+                System.out.println("----------------------------------------");
+                System.out.println("Should dog be trained or not trained?" +
+                        "\nTrained - 1, NoTrained - 2");
+                System.out.println("----------------------------------------");
+                String intellect = scan.nextLine();
+                switch (intellect) {
+                    case "1" -> { dogInfo[3] = new Trained().intellect(); gate = true; }
+                    case "2" -> { dogInfo[3] = new NoTrained().intellect(); gate = true; }
+                    default -> System.out.println("You need to choose between 1 and 2");
+                }
+                if (gate) break;
             }
             System.out.println("----------------------------------------");
             dog.defineTypeOfDog(dogInfo);
             System.out.println("----------------------------------------");
-            System.out.println("Continue? \nYES - y, NO - q or any number");
+            System.out.println("Continue? \nYES - y, NO - q or any character");
             String exit = scan.nextLine();
             if (!exit.equalsIgnoreCase("y")) break;
         }
